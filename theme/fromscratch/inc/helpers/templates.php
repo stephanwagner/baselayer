300687da19520b725d94edcb4491a97dda50bc11
Stephan Wagner <stephanwagner.me@gmail.com>
2026-05-16 23:36:47 +1000

    Add navigation icons

--- theme/fromscratch/inc/helpers/templates.php
+++ theme/fromscratch/inc/helpers/templates.php
@@ -66,11 +66,51 @@ function fs_paginate_links_args_for_wp_query(\WP_Query $query, array $overrides 
 			'current'   => $current,
 			'mid_size'  => 0, // TODO from config
 			'end_size'  => 1,
-			'prev_text' => __('Previous', 'fromscratch'),
-			'next_text' => __('Next', 'fromscratch'),
+			'prev_text' => fs_pagination_nav_link_text('prev'),
+			'next_text' => fs_pagination_nav_link_text('next'),
 			'type'      => 'list',
 		],
 		$overrides
+	);
+}
+
+/**
+ * Accessible prev/next label for {@see paginate_links()}: visible icon, text for screen readers.
+ *
+ * @param 'prev'|'next' $direction
+ */
+function fs_pagination_nav_link_text(string $direction): string
+{
+	$label = $direction === 'prev'
+		? __('Previous page', 'fromscratch')
+		: __('Next page', 'fromscratch');
+
+	return sprintf(
+		'<span class="screen-reader-text">%s</span><span class="pagination__icon" aria-hidden="true">%s</span>',
+		esc_html($label),
+		fs_pagination_icon_svg($direction)
+	);
+}
+
+/**
+ * Chevron SVG for pagination prev/next (matches ACF slider controls).
+ *
+ * @param 'prev'|'next' $direction
+ */
+function fs_pagination_icon_svg(string $direction): string
+{
+	$paths = [
+		'prev' => 'm287.46-450 131.69 131.69q8.93 8.93 8.81 20.89-.11 11.96-8.81 21.27-9.3 9.3-21.38 9.61-12.08.31-21.38-9L197.23-454.69q-10.84-10.85-10.84-25.31 0-14.46 10.84-25.31l179.16-179.15q8.92-8.92 21.19-8.81 12.27.12 21.57 9.42 8.7 9.31 9 21.08.31 11.77-9 21.08L287.46-510h470.62q12.77 0 21.38 8.62 8.62 8.61 8.62 21.38t-8.62 21.38q-8.61 8.62-21.38 8.62H287.46Z',
+		'next' => 'M664.46-450H210q-12.77 0-21.38-8.62Q180-467.23 180-480t8.62-21.38Q197.23-510 210-510h454.46L532.77-641.69q-8.92-8.93-8.81-20.89.12-11.96 8.81-21.27 9.31-9.3 21.38-9.61 12.08-.31 21.39 9l179.15 179.15q5.62 5.62 7.92 11.85 2.31 6.23 2.31 13.46t-2.31 13.46q-2.3 6.23-7.92 11.85L575.54-275.54q-8.93 8.92-21.19 8.81-12.27-.12-21.58-9.42-8.69-9.31-9-21.08-.31-11.77 9-21.08L664.46-450Z',
+	];
+
+	if (!isset($paths[$direction])) {
+		return '';
+	}
+
+	return sprintf(
+		'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -960 960 960" fill="currentColor" focusable="false"><path d="%s"/></svg>',
+		esc_attr($paths[$direction])
 	);
 }
 
