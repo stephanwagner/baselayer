2599380e8884073d22621f044c3140d1e944a1ce
Stephan Wagner <stephanwagner.me@gmail.com>
2026-05-24 22:49:19 +1000

    Add event date to page

--- theme/fromscratch/single.php
+++ theme/fromscratch/single.php
@@ -10,8 +10,14 @@
 			while (have_posts()) {
 				the_post();
 
-				if (fs_page_should_show_title((int) get_the_ID())) {
+				$post_id = (int) get_the_ID();
+
+				if (fs_page_should_show_title($post_id)) {
 					echo '<h1>' . esc_html(get_the_title()) . '</h1>';
+				}
+
+				if (function_exists('fs_is_event_post_type') && fs_is_event_post_type(get_post_type())) {
+					fs_render_template('event-date', ['post_id' => $post_id]);
 				}
 
 				the_content();
