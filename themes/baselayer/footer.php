e4786fe94343f20375ceae32c8a90a7f9c5ef199
Stephan Wagner <stephanwagner.me@gmail.com>
2026-08-31 12:05:04 +1200

    Reduce footer style specifity

--- themes/baselayer/footer.php
+++ themes/baselayer/footer.php
@@ -1,59 +1,61 @@
 <footer class="footer__wrapper">
 	<div class="footer__container container">
-		<div class="footer__text">
-			<?php
-			$company_name = '';
-			$company_address = '';
-			$company_phone = '';
-			$company_email = '';
+		<div class="footer__content">
+			<div class="footer__text">
+				<?php
+				$company_name = '';
+				$company_address = '';
+				$company_phone = '';
+				$company_email = '';
 
-			// ACF
-			// Remove this section if you don't use ACF
-			if (function_exists('bl_theme_blocks_system') && bl_theme_blocks_system() === 'acf' && function_exists('get_field')) {
-				$company = get_field('company', 'option');
-				$company = is_array($company) ? $company : [];
-				$company_name = (string) ($company['name'] ?? '');
-				$company_address = (string) ($company['address'] ?? '');
-				$company_phone = (string) ($company['phone'] ?? '');
-				$company_email = (string) ($company['email'] ?? '');
+				// ACF
+				// Remove this section if you don't use ACF
+				if (function_exists('bl_theme_blocks_system') && bl_theme_blocks_system() === 'acf' && function_exists('get_field')) {
+					$company = get_field('company', 'option');
+					$company = is_array($company) ? $company : [];
+					$company_name = (string) ($company['name'] ?? '');
+					$company_address = (string) ($company['address'] ?? '');
+					$company_phone = (string) ($company['phone'] ?? '');
+					$company_email = (string) ($company['email'] ?? '');
 
-			// BaseLayer Blocks
-			// Remove this section if you don't use BaseLayer Blocks
-			} elseif (function_exists('bl_website_field')) {
-				$company_name = (string) (bl_website_field('general', 'name') ?? '');
-				$company_address = (string) (bl_website_field('general', 'address') ?? '');
-				$company_phone = (string) (bl_website_field('general', 'phone') ?? '');
-				$company_email = (string) (bl_website_field('general', 'email') ?? '');
-			}
-			?>
-			<?php if ($company_name !== '') : ?>
-				<b><?= esc_html($company_name) ?></b><br>
-			<?php endif; ?>
-			<?php if ($company_address !== '') : ?>
-				<?= nl2br(esc_html($company_address)) ?><br>
-			<?php endif; ?>
-			<?php if ($company_phone !== '') : ?>
-				<a href="tel:<?= esc_attr(preg_replace('/\s+/', '', $company_phone)) ?>"><?= esc_html($company_phone) ?></a><br>
-			<?php endif; ?>
-			<?php if ($company_email !== '') : ?>
-				<a href="mailto:<?= esc_attr($company_email) ?>"><?= esc_html($company_email) ?></a>
-			<?php endif; ?>
-			<?php
-			if (function_exists('bl_get_social_media_links') && function_exists('bl_render_template')) {
-				$social_links = bl_get_social_media_links();
-				if ($social_links !== []) {
-					bl_render_template('social-media-links', ['links' => $social_links]);
+				// BaseLayer Blocks
+				// Remove this section if you don't use BaseLayer Blocks
+				} elseif (function_exists('bl_website_field')) {
+					$company_name = (string) (bl_website_field('general', 'name') ?? '');
+					$company_address = (string) (bl_website_field('general', 'address') ?? '');
+					$company_phone = (string) (bl_website_field('general', 'phone') ?? '');
+					$company_email = (string) (bl_website_field('general', 'email') ?? '');
 				}
-			}
-			?>
-		</div>
-		<div class="footer-menu__wrapper">
-			<?php bl_nav_menu([
-				'theme_location' => 'footer_menu',
-				'menu_class' => 'footer-menu__container',
-				'container' => 'nav',
-				'container_aria_label' => esc_attr__('Footer navigation', 'baselayer'),
-			]); ?>
+				?>
+				<?php if ($company_name !== '') : ?>
+					<b><?= esc_html($company_name) ?></b><br>
+				<?php endif; ?>
+				<?php if ($company_address !== '') : ?>
+					<?= nl2br(esc_html($company_address)) ?><br>
+				<?php endif; ?>
+				<?php if ($company_phone !== '') : ?>
+					<a href="tel:<?= esc_attr(preg_replace('/\s+/', '', $company_phone)) ?>"><?= esc_html($company_phone) ?></a><br>
+				<?php endif; ?>
+				<?php if ($company_email !== '') : ?>
+					<a href="mailto:<?= esc_attr($company_email) ?>"><?= esc_html($company_email) ?></a>
+				<?php endif; ?>
+				<?php
+				if (function_exists('bl_get_social_media_links') && function_exists('bl_render_template')) {
+					$social_links = bl_get_social_media_links();
+					if ($social_links !== []) {
+						bl_render_template('social-media-links', ['links' => $social_links]);
+					}
+				}
+				?>
+			</div>
+			<div class="footer-menu__wrapper">
+				<?php bl_nav_menu([
+					'theme_location' => 'footer_menu',
+					'menu_class' => 'footer-menu__container',
+					'container' => 'nav',
+					'container_aria_label' => esc_attr__('Footer navigation', 'baselayer'),
+				]); ?>
+			</div>
 		</div>
 	</div>
 </footer>
